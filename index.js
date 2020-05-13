ea9cece9446aff8b66b9f1ea8e2a1ebdb99c2184
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-05-13 12:47:43 -0600

    Set up express server

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const port = process.env.PORT || 27489;
+
+const server = require("./server.js");
+
+server.listen(port, () => console.log(` == server is listening on port http://127.0.0.1:${port} == `));
