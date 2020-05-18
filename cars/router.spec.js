8cee6a425e87c1f8609abc15048115dd8d3a24f9
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-05-18 11:25:56 -0600

    Add some more tests for cars and make/model/year
    Co-authored-by: Steven Wang <stevenwang2060@gmail.com>
    Co-authored-by: gunnarthorson <willow.avenue.decor@gmail.com>

--- cars/router.spec.js
+++ cars/router.spec.js
@@ -1,9 +1,43 @@
 const request = require("supertest");
 const server = require("../server.js");
 
+const db = require("../data/dbconfig.js");
+
 describe("GET /api/cars", () => {
-    it("should return json type", async () => {
-        const res = await request(server).get("/api/cars");
-        expect(res.type).toBe("application/json");
-    })
-})
+  const example_car = {
+    id: 1,
+    make: "Tesla",
+    model: "X",
+    year: 2018,
+  };
+
+  beforeEach(() => {
+    return db("epa_vehicles_all")
+      .truncate()
+      .then(() => db("epa_vehicles_all").insert(example_car));
+  });
+
+  it("should return json type", async () => {
+    const res = await request(server).get("/api/cars");
+    expect(res.type).toBe("application/json");
+  });
+
+  it("Should return status of 200", async () => {
+    const res = await request(server).get("/api/cars");
+    expect(res.status).toBe(200);
+  });
+
+  it("Should return status of 200", async () => {
+    return await request(server)
+      .get("/api/cars/1")
+      .then((res) => {
+        expect(res.status).toBe(200);
+        expect(res.body).toEqual(example_car);
+      });
+  });
+
+  it("Should return status of 404", async () => {
+    const res = await request(server).get("/api/cars/1337");
+    expect(res.status).toBe(404);
+  });
+});
--- server.spec.js
+++ server.spec.js
@@ -1,30 +1,56 @@
 const request = require("supertest");
-const server = require('./server');
+const server = require("./server");
 const db = require("./data/dbconfig.js");
 
-
-
-describe('test server.js', () => {
-    it(' should set the testing environment', () => {
-        expect(true).toBe(true);
-    })
+describe("test server.js", () => {
+  it(" should set the testing environment", () => {
+    expect(true).toBe(true);
+  });
 });
-    it('Should return the following message', function() {
-        return request(server).get('/').then(res => {
-            expect(res.body).toEqual({ message: "Server up and running" });
-        })
-    })
 
+it("Should return the following message", function () {
+  return request(server)
+    .get("/")
+    .then((res) => {
+      expect(res.body).toEqual({ message: "Server up and running" });
+    });
+});
 
-    describe('GET /', function() {
-        it('Should return status of 200', function(){
-            return request(server).get('/').then(res => {
-                expect(res.status).toBe(200);
-            })
-        })
-        it('Should return JSON', function() {
-            return request(server).get('/').then(res => {
-                expect(res.type).toMatch(/json/i);
-            })
-        })
-    })
+describe("GET /", function () {
+  it("Should return status of 200", function () {
+    return request(server)
+      .get("/")
+      .then((res) => {
+        expect(res.status).toBe(200);
+      });
+  });
+
+  it("Should return JSON", function () {
+    return request(server)
+      .get("/")
+      .then((res) => {
+        expect(res.type).toMatch(/json/i);
+      });
+  });
+});
+
+describe("GET /api/model", function () {
+  it("Should return status of 200 for model", async () => {
+    const res = await request(server).get("/api/model");
+    expect(res.status).toBe(200);
+  });
+});
+
+describe("GET /api/make", function () {
+  it("Should return status of 200 for make", async () => {
+    const res = await request(server).get("/api/make");
+    expect(res.status).toBe(200);
+  });
+});
+
+describe("GET /api/year", function () {
+  it("Should return status of 200", async () => {
+    const res = await request(server).get("/api/year");
+    expect(res.status).toBe(200);
+  });
+});
