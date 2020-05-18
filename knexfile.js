36172a82c2432d2fbf9d3a68c7c2a50b53795eec
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-05-18 13:39:13 -0600

    Switch to using AWS database

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,27 @@
 // Update with your config settings.
 module.exports = {
-
   testing: {
-    client: 'sqlite',
+    client: "sqlite",
     useNullAsDefault: true,
     connection: {
       filename: "./data/testing.db",
     },
     migrations: {
-      directory: "./data/migrations"
-    }
+      directory: "./data/migrations",
+    },
   },
-  
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: "./data/migrations"
-    }
-  }
 
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST_AWS,
+      user: process.env.DB_USER_AWS,
+      password: process.env.DB_PW_AWS,
+      port: process.env.DB_PORT_AWS,
+      database: process.env.DB_NAME_AWS,
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+  },
 };
