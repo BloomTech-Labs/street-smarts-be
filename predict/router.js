8b52dc43315137d997ba3d9040596e6d29fc2462
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-05-26 10:08:28 -0600

    Send back more information with co2 prediction
    Co-authored-by: gunnarthorson <willow.avenue.decor@gmail.com>
    Co-authored-by: Steven Wang <stevenwang2060@gmail.com>

--- predict/router.js
+++ predict/router.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const API =
   "http://streetsmarts-ds-labs24.eba-dsb2aeqx.us-east-1.elasticbeanstalk.com";
 
-//POST /api/cars/carbon_emissions
+//POST /api/predict/carbon_emissions
 router.post("/carbon_emissions", (req, res) => {
   if (!req.query.car) {
     res.status(400).json({ message: "car query parameter must be defined" });
@@ -24,7 +24,14 @@ router.post("/carbon_emissions", (req, res) => {
             `${API}/carbon_emissions2?make=${cars.make}&model=${cars.model}&year=${cars.year}`
           )
           .then((carbon_emissions_prediction) => {
-            res.status(200).json(carbon_emissions_prediction.data);
+            res.status(200).json({
+              id,
+              make: cars.id,
+              model: cars.model,
+              year: cars.year,
+              predicted_carbon_emissions:
+                carbon_emissions_prediction.data.predicted_co2_sql,
+            });
           })
           .catch((err) => {
             console.log(err);
