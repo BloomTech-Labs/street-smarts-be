3ff4e74f945b7bf047d4e21b573a2e668bdbecd1
Steven Wang <stevenwang2060@gmail.com>
2020-05-27 09:55:21 -0700

    Added testing for both of the carbon emissions routes

--- predict/router.js
+++ predict/router.js
@@ -9,7 +9,47 @@ const API =
   "http://streetsmarts-ds-labs24.eba-dsb2aeqx.us-east-1.elasticbeanstalk.com";
 
 //POST /api/predict/carbon_emissions
-router.post("/carbon_emissions", async (req, res) => {
+router.post("/carbon_emissions", (req, res) => {
+  if (!req.query.car) {
+    res.status(400).json({ message: "car query parameter must be defined" });
+    return;
+  }
+  const id = req.query.car;
+
+  Cars.searchById(id)
+    .then((cars) => {
+      if (cars) {
+        axios
+          .post(
+            `${API}/carbon_emissions2?make=${cars.make}&model=${cars.model}&year=${cars.year}`
+          )
+          .then((carbon_emissions_prediction) => {
+            res.status(200).json({
+              id,
+              make: cars.id,
+              model: cars.model,
+              year: cars.year,
+              predicted_carbon_emissions:
+                carbon_emissions_prediction.data.predicted_co2_sql,
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json({ message: "Failed to get prediction" });
+          });
+      } else {
+        console.log(cars);
+        res.status(404).json({ message: "Couldn't find car with given ID" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Failed to get cars" });
+    });
+});
+
+//POST /api/predict/carbon_emissions
+router.post("/carbon_emissions2", async (req, res) => {
   if (!req.body || !Array.isArray(req.body)) {
     console.log(req.body);
     res.status(400).json({ message: "list of car id's must be given" });
---
+++ predict/router.spec.js
@@ -0,0 +1,50 @@
+const request = require("supertest");
+const server = require("../server.js");
+
+const db = require("../data/dbconfig.js");
+
+describe("POST /api/predict/carbon_emissions", () => {
+    it("add car ID", async () => {
+        const res = await request(server)
+            .post("/api/predict/carbon_emissions?car=1");
+            console.log(res.body);
+        expect(res.status).toBe(200);
+    })
+
+    it("POST fail", async () => {
+        const res = await request(server)
+            .post("/api/predict/carbon_emissions?car=234");
+        expect(res.status).toBe(404);
+    })
+    it("Returns in JSON format", async () => {
+        const res = await request(server)
+        .post("/api/predict/carbon_emissions")
+        .send([1])
+        expect(res.type).toBe("application/json");
+    })
+})
+
+describe("POST /api/predict/carbon_emissions2", () => {
+    it("add car ID", async () => {
+        const carbons = [1];
+        const res = await request(server)
+            .post("/api/predict/carbon_emissions2")
+            .send(carbons)
+        expect(res.status).toBe(200);
+    })
+
+    it("POST fail", async () => {
+        const failure = [1283];
+        const res = await request(server)
+            .post("/api/predict/carbon_emissions2")
+            .send(failure)
+        expect(res.status).toBe(200);
+        expect(res.body[0].status).toBe(404);
+    })
+    it("Returns in JSON format", async () => {
+        const res = await request(server)
+        .post("/api/predict/carbon_emissions2")
+        .send([1])
+        expect(res.type).toBe("application/json");
+    })
+})
