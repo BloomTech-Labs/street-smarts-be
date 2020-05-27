b8cee0ac13e41c0ee4bc2f5a330a52f2803efcf9
Steven Wang <stevenwang2060@gmail.com>
2020-05-27 10:19:51 -0700

    Updated router.js
    Co-authored-by: gunnarthorson <willow.avenue.decor@gmail.com>
    Co-authored-by: LeRoyce Pearson <leroycepearson@geemili.xyz>

--- predict/router.js
+++ predict/router.js
@@ -9,61 +9,49 @@ const API =
   "http://streetsmarts-ds-labs24.eba-dsb2aeqx.us-east-1.elasticbeanstalk.com";
 
 //POST /api/predict/carbon_emissions
-router.post("/carbon_emissions", (req, res) => {
+router.post("/carbon_emissions", async (req, res) => {
   if (!req.query.car) {
-    res.status(400).json({ message: "car query parameter must be defined" });
+    res.status(400).json({
+      message: "car query parameter must be defined"
+    });
     return;
   }
   const id = req.query.car;
 
-  Cars.searchById(id)
-    .then((cars) => {
-      if (cars) {
-        axios
-          .post(
-            `${API}/carbon_emissions2?make=${cars.make}&model=${cars.model}&year=${cars.year}`
-          )
-          .then((carbon_emissions_prediction) => {
-            res.status(200).json({
-              id,
-              make: cars.id,
-              model: cars.model,
-              year: cars.year,
-              predicted_carbon_emissions:
-                carbon_emissions_prediction.data.predicted_co2_sql,
-            });
-          })
-          .catch((err) => {
-            console.log(err);
-            res.status(500).json({ message: "Failed to get prediction" });
-          });
-      } else {
-        console.log(cars);
-        res.status(404).json({ message: "Couldn't find car with given ID" });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ message: "Failed to get cars" });
-    });
+  const predictionRes = await getPredictionForCar(id);
+  if (predictionRes.ok) {
+    delete predictionRes.ok.status;
+    res.status(200).json(predictionRes.ok);
+  } else {
+    res.status(predictionRes.err.status).json(predictionRes.err);
+  }
 });
 
 //POST /api/predict/carbon_emissions
 router.post("/carbon_emissions2", async (req, res) => {
   if (!req.body || !Array.isArray(req.body)) {
     console.log(req.body);
-    res.status(400).json({ message: "list of car id's must be given" });
+    res.status(400).json({
+      message: "list of car id's must be given"
+    });
     return;
   }
   if (req.body.length > 10) {
-    res.status(400).json({ message: "maximum number of car ids is 10" });
+    res.status(400).json({
+      message: "maximum number of car ids is 10"
+    });
     return;
   }
   const queries = req.body.map(getPredictionForCar);
 
   const res_data = [];
   for (let query of queries) {
-    res_data.push(await query);
+    const predictionRes = await query;
+    if (predictionRes.ok) {
+      res_data.push(predictionRes.ok);
+    } else {
+      res_data.push(predictionRes.err);
+    }
   }
 
   res.status(200).json(res_data);
@@ -73,15 +61,27 @@ async function getPredictionForCar(id) {
   const carRes = await Cars.searchById(id)
     .then((car) => {
       if (car) {
-        return { ok: car };
+        return {
+          ok: car
+        };
       } else {
         return {
-          err: { error: "Could not find car with id", id, status: 404 },
+          err: {
+            error: "Could not find car with id",
+            id,
+            status: 404
+          },
         };
       }
     })
     .catch((err) => {
-      return { err: { id, error: err, status: 500 } };
+      return {
+        err: {
+          id,
+          error: err,
+          status: 500
+        }
+      };
     });
 
   if (carRes.ok) {
@@ -92,22 +92,29 @@ async function getPredictionForCar(id) {
       )
       .then((carbon_emissions_prediction) => {
         return {
-          id: car.id,
-          make: car.make,
-          model: car.model,
-          year: car.year,
-          predicted_carbon_emissions:
-            carbon_emissions_prediction.data.predicted_co2_sql,
-          status: 200,
+          ok: {
+            id: car.id,
+            make: car.make,
+            model: car.model,
+            year: car.year,
+            predicted_carbon_emissions: carbon_emissions_prediction.data.predicted_co2_sql,
+            status: 200,
+          }
         };
       })
       .catch((err) => {
         console.log(err);
-        return { id: car.id, message: "Failed to get prediction", status: 500 };
+        return {
+          err: {
+            id: car.id,
+            message: "Failed to get prediction",
+            status: 500
+          }
+        };
       });
     return prediction;
   } else if (carRes.err) {
-    return carRes.err;
+    return carRes;
   }
 }
 
