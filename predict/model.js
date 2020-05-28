7e5a564358fcabf5219321c168f837a2b3ab20ea
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-05-28 10:20:15 -0600

    Break getPredictionForCar into multiple functions

--- predict/model.js
+++ predict/model.js
@@ -2,75 +2,80 @@ const Cars = require("../cars/model");
 const axios = require("axios");
 
 module.exports = {
-    getPredictionForCar,
-    getSinglePrediction
-}
+  getPredictionForCar,
+  getSinglePrediction,
+};
 
 const API =
   "http://streetsmarts-ds-labs24.eba-dsb2aeqx.us-east-1.elasticbeanstalk.com";
 
 async function getPredictionForCar(endpoint, id) {
-    const carRes = await Cars.searchById(id)
-        .then((car) => {
-            if (car) {
-                return {
-                    ok: car
-                };
-            } else {
-                return {
-                    err: {
-                        error: "Could not find car with id",
-                        id,
-                        status: 404
-                    },
-                };
-            }
-        })
-        .catch((err) => {
-            return {
-                err: {
-                    id,
-                    error: err,
-                    status: 500
-                }
-            };
-        });
+  const carRes = await getCarResultById(id);
 
-    if (carRes.ok) {
-        const car = carRes.ok;
-        const prediction = await axios
-            .post(
-                `${API}/${endpoint}?make=${car.make}&model=${car.model}&year=${car.year}`
-            )
-            .then((prediction) => {
-                return {
-                    ok: {
-                        id: car.id,
-                        make: car.make,
-                        model: car.model,
-                        year: car.year,
-                        predicted_carbon_emissions: prediction.data.predicted_co2_sql,
-                        predicted_price: prediction.data.predicted_price,
-                        status: 200,
-                    }
-                };
-            })
-            .catch((err) => {
-                console.log(err);
-                return {
-                    err: {
-                        id: car.id,
-                        message: "Failed to get prediction",
-                        status: 500
-                    }
-                };
-            });
-        return prediction;
-    } else if (carRes.err) {
-        return carRes;
-    }
+  if (carRes.ok) {
+    return await requestPrediction(endpoint, carRes.ok);
+  } else if (carRes.err) {
+    return carRes;
+  }
 }
 
+function getCarResultById(id) {
+  return Cars.searchById(id)
+    .then((car) => {
+      if (car) {
+        return {
+          ok: car,
+        };
+      } else {
+        return {
+          err: {
+            error: "Could not find car with id",
+            id,
+            status: 404,
+          },
+        };
+      }
+    })
+    .catch((err) => {
+      return {
+        err: {
+          id,
+          error: err,
+          status: 500,
+        },
+      };
+    });
+}
+
+function requestPrediction(endpoint, car) {
+  return axios
+    .post(
+      `${API}/${endpoint}?make=${car.make}&model=${car.model}&year=${car.year}`
+    )
+    .then((prediction) => {
+      return {
+        ok: {
+          id: car.id,
+          make: car.make,
+          model: car.model,
+          year: car.year,
+          predicted_carbon_emissions: prediction.data.predicted_co2_sql,
+          predicted_price: prediction.data.predicted_price,
+          status: 200,
+        },
+      };
+    })
+    .catch((err) => {
+      console.log(err);
+      return {
+        err: {
+          id: car.id,
+          message: "Failed to get prediction",
+          status: 500,
+        },
+      };
+    });
+}
 
 function getSinglePrediction(endpoint) {
     return async function (req, res) {
