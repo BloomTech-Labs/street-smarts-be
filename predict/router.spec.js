86071c5500c0dce51e77f5b0d12dbd2d2c177cf5
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-05-28 10:06:17 -0600

    Mock axios calls
    Co-authored-by: Steven Wang <stevenwang2060@gmail.com>
    Co-authored-by: gunnarthorson <willow.avenue.decor@gmail.com>

--- predict/router.spec.js
+++ predict/router.spec.js
@@ -1,14 +1,21 @@
 const request = require("supertest");
 const server = require("../server.js");
+const mockAxios = require("axios");
 
 const db = require("../data/dbconfig.js");
 
 describe("POST /api/predict/carbon_emissions", () => {
+    const data = {
+        predicted_co2_sql: 300,
+    };
+
     it("add car ID", async () => {
+        mockAxios.post.mockImplementationOnce(() => Promise.resolve({ status: 200, data }));
         const res = await request(server)
             .post("/api/predict/carbon_emissions/1");
             console.log(res.body);
         expect(res.status).toBe(200);
+        expect(res.body.predicted_carbon_emissions).toBe(data.predicted_co2_sql);
     })
 
     it("POST fail", async () => {
@@ -50,9 +57,15 @@ describe("POST /api/predict/carbon_emissions2", () => {
 
 describe("POST /api/predict/price", () => {
     it("add car ID", async () => {
+        const data = {
+            predicted_price: 300,
+        };
+
+        mockAxios.post.mockImplementationOnce(() => Promise.resolve({ status: 200, data }));
         const res = await request(server)
             .post("/api/predict/price/1")
         expect(res.status).toBe(200);
+        expect(res.body.predicted_price).toBe(data.predicted_price);
     })
 
     it("POST fail", async () => {
