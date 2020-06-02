1aa51cf7f250fa1011205935aa835365ec90eec3
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-06-02 10:58:41 -0600

    Repurpose `/predict/price` tests as `/predict` tests

--- predict/router.spec.js
+++ predict/router.spec.js
@@ -55,28 +55,33 @@ describe("POST /api/predict/carbon_emissions2", () => {
     })
 })
 
-describe("POST /api/predict/price", () => {
-    it("add car ID", async () => {
+describe("POST /api/predict", () => {
+    it("should return a prediction for the car price", async () => {
         const data = {
-            predicted_price: 300,
+          car_price_prediction: 2873.39,
+          fuel_cost: 12500,
+          maintenance_cost: 5000,
+          five_year_cost_to_own: 20373.39,
+          co2_five_year_kgs: 37029.17,
+          number_of_trees_to_offset: 340
         };
 
         mockAxios.post.mockImplementationOnce(() => Promise.resolve({ status: 200, data }));
         const res = await request(server)
-            .post("/api/predict/price/1")
+            .post("/api/predict/1")
         expect(res.status).toBe(200);
-        expect(res.body.predicted_price).toBe(data.predicted_price);
+        expect(res.body.predicted_price).toBe(data.car_price_prediction);
     })
 
     it("POST fail", async () => {
         const res = await request(server)
-            .post("/api/predict/price/234")
+            .post("/api/predict/234")
         expect(res.status).toBe(404);
     })
 
     it("Returns in JSON format", async () => {
         const res = await request(server)
-        .post("/api/predict/price/1")
+        .post("/api/predict/1")
         expect(res.type).toBe("application/json");
     })
 })
