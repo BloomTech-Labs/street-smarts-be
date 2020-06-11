2810a88233f96bb625700ddb4f05980ac4bf7b46
Steven Wang <stevenwang2060@gmail.com>
2020-06-11 13:58:04 -0700

    Removed carbon_emissions endpoints

--- predict/router.js
+++ predict/router.js
@@ -1,43 +1,9 @@
 const express = require("express");
 
 const router = express.Router();
-const { getPredictionForCar, getSinglePrediction } = require('./model');
+const { getSinglePrediction } = require('./model');
 
-//POST /api/predict/carbon_emissions
-router.post("/carbon_emissions/:id", getSinglePrediction("carbon_emissions2"));
-
-//POST /api/predict/carbon_emissions
-router.post("/carbon_emissions2", async (req, res) => {
-  if (!req.body || !Array.isArray(req.body)) {
-    console.log(req.body);
-    res.status(400).json({
-      message: "list of car id's must be given"
-    });
-    return;
-  }
-  if (req.body.length > 10) {
-    res.status(400).json({
-      message: "maximum number of car ids is 10"
-    });
-    return;
-  }
-  const queries = req.body.map(id => getPredictionForCar("carbon_emissions2", id));
-
-  const res_data = [];
-  for (let query of queries) {
-    const predictionRes = await query;
-    if (predictionRes.ok) {
-      res_data.push(predictionRes.ok);
-    } else {
-      res_data.push(predictionRes.err);
-    }
-  }
-
-  res.status(200).json(res_data);
-});
-
-
-//POST /api/predict/carbon_emissions
+//POST /api/predict
 router.post("/:id", getSinglePrediction("predict"));
 
 module.exports = router;
--- predict/router.spec.js
+++ predict/router.spec.js
@@ -4,57 +4,6 @@ const mockAxios = require("axios");
 
 const db = require("../data/dbconfig.js");
 
-describe("POST /api/predict/carbon_emissions", () => {
-    const data = {
-        predicted_co2_sql: 300,
-    };
-
-    it("add car ID", async () => {
-        mockAxios.post.mockImplementationOnce(() => Promise.resolve({ status: 200, data }));
-        const res = await request(server)
-            .post("/api/predict/carbon_emissions/1");
-            console.log(res.body);
-        expect(res.status).toBe(200);
-        expect(res.body.predicted_carbon_emissions).toBe(data.predicted_co2_sql);
-    })
-
-    it("POST fail", async () => {
-        const res = await request(server)
-            .post("/api/predict/carbon_emissions/234");
-        expect(res.status).toBe(404);
-    })
-    it("Returns in JSON format", async () => {
-        const res = await request(server)
-        .post("/api/predict/carbon_emissions/1")
-        expect(res.type).toBe("application/json");
-    })
-})
-
-describe("POST /api/predict/carbon_emissions2", () => {
-    it("add car ID", async () => {
-        const carbons = [1];
-        const res = await request(server)
-            .post("/api/predict/carbon_emissions2")
-            .send(carbons)
-        expect(res.status).toBe(200);
-    })
-
-    it("POST fail", async () => {
-        const failure = [1283];
-        const res = await request(server)
-            .post("/api/predict/carbon_emissions2")
-            .send(failure)
-        expect(res.status).toBe(200);
-        expect(res.body[0].status).toBe(404);
-    })
-    it("Returns in JSON format", async () => {
-        const res = await request(server)
-        .post("/api/predict/carbon_emissions2")
-        .send([1])
-        expect(res.type).toBe("application/json");
-    })
-})
-
 describe("POST /api/predict", () => {
     it("should return a prediction for the car price", async () => {
         const data = {
